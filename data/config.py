b3c6435e3e0ffbffd02c76794f04b8be42bdb65a
godofprog <stepanderdichok@gmail.com>
2022-02-21 23:36:12 +0300

    Final_fix2

--- data/config.py
+++ data/config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = '5070398455:AAEUQYeWBBRGEj5t46eueF-dxbgrazTQR94'
-ADMINS = ['961406924', '518091887']
+ADMINS = ['961406924', '518091887', '1315635438']
 DB_URI = 'postgres://fxefhpkhakzquo:47c73bbfc63473b52c34b3f5b2c8c55332ac6c3bdabe3c89f3e570b19e744e29@ec2-34-255-109-139' \
          '.eu-west-1.compute.amazonaws.com:5432/dchl5fiq1d2lka'
 IP = 'localhost'
